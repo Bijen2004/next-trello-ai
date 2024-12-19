380da39211db44d1480d6df77e64b11d0676c8ce
Bijen Shrestha <shrbijen31@gmail.com>
2024-12-19 20:53:20 +0545

    trello clone

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Board from '@/components/Board'
+import Header from '@/components/Header'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <Board />
+    </div>
+  )
+}
+
+export default Homepage
